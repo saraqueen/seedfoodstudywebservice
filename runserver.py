031392cc37703fb6c043298b9c96a52b2517c827
Jinay Mehta <mjinay@gmail.com>
2014-04-30 19:47:37 -0400

    app run change
    app run change

--- runserver.py
+++ runserver.py
@@ -1,2 +1,3 @@
 from seedfoodstudywebservice import app
-app.run(host=os.environ['OPENSHIFT_PYTHON_IP'],port=int(os.environ['OPENSHIFT_PYTHON_PORT']),debug='true')
+app.run()
+#app.run(host=os.environ['OPENSHIFT_PYTHON_IP'],port=int(os.environ['OPENSHIFT_PYTHON_PORT']),debug='true')
